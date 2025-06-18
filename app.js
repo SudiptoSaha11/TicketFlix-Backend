4fcaa29185eec3ac5470a58e71a43013f61ee0d8
Sudipto <Sudiptosaha1111@gmail.com>
2025-06-18 11:43:28 +0530

    success

--- app.js
+++ app.js
@@ -64,8 +64,8 @@ app.post('/api/create-checkout-session', async (req, res) => {
       payment_method_types: ['card'],
       line_items: lineItems,
       mode: 'payment',
-      success_url: 'http://localhost:3000/success',
-      cancel_url: 'http://localhost:3000/cancel',
+      success_url: 'https://ticketflix-backend.onrender.com/success',
+      cancel_url: 'https://ticketflix-backend.onrender.com/cancel',
       metadata: {
         bookingDetails: JSON.stringify({
           movieName: Name,
